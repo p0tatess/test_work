735df4ccbe98cf93ea38883061635e3c9f32455f
p0tatess <p0tates322@gmail.com>
2023-04-26 15:11:41 +0700

    fix names

--- quest1/asgi.py
+++ quest1/asgi.py
@@ -1,5 +1,4 @@
 """
-ASGI config for asdasd project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asdasd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quest1.settings')
 
 application = get_asgi_application()
