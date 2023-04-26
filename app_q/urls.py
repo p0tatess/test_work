cb34306b02b48beef78504f5d02f95d97224b67c
p0tatess <p0tates322@gmail.com>
2023-04-26 15:02:55 +0700

    redirection

---
+++ app_q/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.base, name='base'),
+    path('add', views.add_article, name='add'),
+    path('post/<int:post_id>/', views.show_post, name ='post'),
+    path('update/<int:post_id>/', views.update, name='update'),
+    path('delete/<int:post_id>/', views.delete, name='delete'),
+
+]
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quest1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asdasd.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
